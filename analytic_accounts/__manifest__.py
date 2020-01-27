69d18ccc64a39baeb2ba6b6c54e468dc25812a79
LuisMora <LuisMora@DESKTOP-90891QA>
2020-01-27 10:51:36 +0100

    añadido pagina web, description y version beta

--- analytic_accounts/__manifest__.py
+++ analytic_accounts/__manifest__.py
@@ -7,17 +7,19 @@
     reports """,
 
     'description': """
-        Long description of module's purpose
+        An improvement in Analyitic Accounts,
+        by adding Graphics, filter, import/export automation and
+        reports 
     """,
 
     'author': "Eduweb group",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.eduwebgroup.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Accounting',
+    'version': '0.1-beta',
 
     # any module necessary for this one to work correctly
     'depends': ['base','account_accountant'],
