8e98cb002c00957ac2f4fa007b3ffafb730cad9f
LuisMalave2001 <luisangelmalavemora@gmail.com>
2020-02-18 16:41:29 +0100

    Analytic added
    Commit

---
+++ analytic_accounts/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Analytic Accounts Improved",
+
+    'summary': """ An improvement in Analyitic Accounts,
+    by adding Graphics, filter, import/export automation and
+    reports """,
+
+    'description': """
+        An improvement in Analyitic Accounts,
+        by adding Graphics, filter, import/export automation and
+        reports 
+    """,
+
+    'author': "Eduweb group",
+    'website': "http://www.eduwebgroup.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.1-beta',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account_accountant'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+        "views/analytic_account_views.xml",
+        "views/analytic_groups_views.xml",
+        "data/reports.xml",
+        "data/data_menu.xml",
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
